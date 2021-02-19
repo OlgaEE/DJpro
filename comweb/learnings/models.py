cecfcbd620dbdeebc062053961aacf3588b0a970
Olga <olgtit@hot.ee>
2021-02-19 20:56:24 +0200

    Add template tags

--- comweb/learnings/models.py
+++ comweb/learnings/models.py
@@ -17,6 +17,10 @@ class Learnings(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        ordering = ['-time_create', 'title']
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
 
@@ -25,3 +29,6 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        ordering = ['id']
--- comweb/learnings/views.py
+++ comweb/learnings/views.py
@@ -11,10 +11,9 @@ menu = [{'title': "About Sait", 'url_name': 'about'},
 
 def index(request):
     posts = Learnings.objects.all()
-    cats = Category.objects.all()
+
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Main Page',
         'cat_selected': 0,
@@ -47,14 +46,12 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Learnings.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Swoing Category',
         'cat_selected': cat_id,
