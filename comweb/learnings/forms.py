837f2362da9a2def7ac93cd814a9b88823999726
Olga <olgtit@hot.ee>
2021-03-14 15:30:25 +0200

    Add form connected with model

--- comweb/learnings/forms.py
+++ comweb/learnings/forms.py
@@ -1,11 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
-class AddPostForm(forms.Form):
-    class AddPostForm(forms.Form):
-        title = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-input'}))
-        slug = forms.SlugField(max_length=255, label="URL", widget=forms.TextInput(attrs={'class': 'form-input'}))
-        content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-        is_published = forms.BooleanField(label="Published", required=False, initial=True)
-        cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Category", empty_label="Category is not choosen")
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Category is not choosen"
 
+    class Meta:
+        model = Learnings
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'row': 10}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('The length is more than 200 characters')
+        return title
